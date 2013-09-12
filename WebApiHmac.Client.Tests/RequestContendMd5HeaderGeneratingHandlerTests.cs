4b92b76f4466289a8831459bcf61286173733fa9
Alex Brown <alex.brown@collinsontech.com>
2013-09-12 11:19:44 +0100

    Started Client side - Created handler to set content md5 header of request
    added tests

---
+++ WebApiHmac.Client.Tests/RequestContendMd5HeaderGeneratingHandlerTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace WebApiHmac.Client.Tests
+{
+    [TestFixture]
+    public class RequestContendMd5HeaderGeneratingHandlerTests
+    {
+        private RequestContendMd5HeaderGeneratingHandler handler;
+        private HttpClient client;
+
+        [SetUp]
+        public void SetUp()
+        {
+            handler = new RequestContendMd5HeaderGeneratingHandler { InnerHandler = new TestHandler() };
+
+            client = new HttpClient(handler);
+        }
+
+        [Test]
+        public void generates_md5_header_and_saves_to_request()
+        {
+
+            var request = new HttpRequestMessage(HttpMethod.Get, "http://www.test.com")
+                                     {
+                                         Content = new StringContent("something")
+                                     };
+
+            var expectedMd5 = new System.Security.Cryptography.MD5CryptoServiceProvider()
+                .ComputeHash(Encoding.UTF8.GetBytes("something"));
+
+            var result = client.SendAsync(request)
+                .Result;
+
+            Assert.That(result.StatusCode, Is.EqualTo(HttpStatusCode.OK));
+
+            var resultMd5 = request.Content
+                .Headers
+                .ContentMD5;
+
+            Assert.IsTrue(resultMd5.SequenceEqual(expectedMd5));
+        }
+    }
+}
---
+++ WebApiHmac.Client/RequestContendMd5HeaderGeneratingHandler.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace WebApiHmac.Client
+{
+    public class RequestContendMd5HeaderGeneratingHandler : DelegatingHandler
+    {
+        protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request,
+                                                               System.Threading.CancellationToken cancellationToken)
+        {
+            var contentBytes = request.Content
+                .ReadAsByteArrayAsync()
+                .Result;
+
+            var contentMD5 = new System.Security.Cryptography.MD5CryptoServiceProvider()
+                .ComputeHash(contentBytes);
+
+            request.Content.Headers.ContentMD5 = contentMD5;
+
+            var response = await base.SendAsync(request, cancellationToken);
+
+            return response;
+        }
+    }
+}
