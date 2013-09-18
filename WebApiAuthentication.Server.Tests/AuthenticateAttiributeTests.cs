f03cd03ff54216e058790f3fa48d4f742af57137
Alex Brown <alex.brown@collinsontech.com>
2013-09-18 14:46:37 +0100

    green tests on Authenticate attribute Needs refactoring

--- WebApiAuthentication.Server.Tests/AuthenticateAttiributeTests.cs
+++ WebApiAuthentication.Server.Tests/AuthenticateAttiributeTests.cs
@@ -1,4 +1,6 @@
 using System.Net;
+using System.Net.Http.Headers;
+using Moq;
 using NUnit.Framework;
 using WebApiAuthentication.Tests;
 
@@ -7,6 +9,30 @@ namespace WebApiAuthentication.Server.Tests
     [TestFixture]
     public class AuthenticateAttiributeTests
     {
+        private AuthenticateAttiribute attribute;
+
+        [SetUp]
+        public void SetUp()
+        {
+            attribute = new AuthenticateAttiribute();
+        }
+
+        [Test]
+        public void returns_not_authorised_for_requests_without_username_header()
+        {
+            var request = HttpRequestMessageBuilder.Instance().Build();
+
+            var actionContext = HttpActionContextBuilder
+                .Instance()
+                .WithRequestMessage(request)
+                .Build();
+
+            attribute.OnActionExecuting(actionContext);
+
+            Assert.That(actionContext.Response, Is.Not.Null);
+            Assert.That(actionContext.Response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
         [Test]
         public void returns_not_authorised_for_requests_without_authentication_header()
         {
@@ -18,12 +44,84 @@ namespace WebApiAuthentication.Server.Tests
                 .WithRequestMessage(request)
                 .Build();
 
-            var attribute = new AuthenticateAttiribute();
+            attribute.OnActionExecuting(actionContext);
+
+            Assert.That(actionContext.Response, Is.Not.Null);
+            Assert.That(actionContext.Response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public void returns_not_authorised_if_retrieved_secret_is_null()
+        {
+            var mockGetSecretFromUsername = new Mock<IGetSecretFromUsername>();
+            mockGetSecretFromUsername.Setup(x => x.Secret(It.IsAny<string>()))
+                .Returns((string)null);
+
+            var request = HttpRequestMessageBuilder.Instance().Build();
+            request.Headers.Authorization = new AuthenticationHeaderValue(HeaderNames.AuthenticationScheme, "signature_hash");
+
+            var actionContext = HttpActionContextBuilder
+                .Instance()
+                .WithRequestMessage(request)
+                .Build();
+
+            attribute.GetSecretFromUsername = mockGetSecretFromUsername.Object;
+            attribute.OnActionExecuting(actionContext);
+
+            Assert.That(actionContext.Response, Is.Not.Null);
+            Assert.That(actionContext.Response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public void returns_not_authorised_if_signatures_dont_match()
+        {
+            var mockGetSecretFromUsername = new Mock<IGetSecretFromUsername>();
+            mockGetSecretFromUsername.Setup(x => x.Secret(It.IsAny<string>()))
+                .Returns("secret");
+
+            var request = HttpRequestMessageBuilder.Instance().Build();
+            request.Headers.Authorization = new AuthenticationHeaderValue(HeaderNames.AuthenticationScheme, "signature_hash");
+            request.Headers.Add(HeaderNames.UsernameHeader, "username");
+
+            var actionContext = HttpActionContextBuilder
+                .Instance()
+                .WithRequestMessage(request)
+                .Build();
+
+            attribute.GetSecretFromUsername = mockGetSecretFromUsername.Object;
 
             attribute.OnActionExecuting(actionContext);
 
             Assert.That(actionContext.Response, Is.Not.Null);
             Assert.That(actionContext.Response.StatusCode, Is.EqualTo(HttpStatusCode.Unauthorized));
+        }
+
+        [Test]
+        public void passes_through_if_signatures_match()
+        {
+            var mockGetSecretFromUsername = new Mock<IGetSecretFromUsername>();
+            mockGetSecretFromUsername.Setup(x => x.Secret(It.IsAny<string>()))
+                .Returns("secret");
+
+            var request = HttpRequestMessageBuilder.Instance().Build();
+            request.Headers.Authorization = new AuthenticationHeaderValue(HeaderNames.AuthenticationScheme, "signature_hash");
+            request.Headers.Add(HeaderNames.UsernameHeader, "username");
+
+            var mockBuildRequestSignature = new Mock<IBuildRequestSignature>();
+            mockBuildRequestSignature.Setup(x => x.Build("secret", request))
+                .Returns("signature_hash");
+
+            var actionContext = HttpActionContextBuilder
+                .Instance()
+                .WithRequestMessage(request)
+                .Build();
+
+            attribute.GetSecretFromUsername = mockGetSecretFromUsername.Object;
+            attribute.BuildRequestSignature = mockBuildRequestSignature.Object;
+
+            attribute.OnActionExecuting(actionContext);
+
+            Assert.That(actionContext.Response, Is.Null); //for purposes of this test, for now
         }
     }
 }
--- WebApiAuthentication.Server/AuthenticateAttiribute.cs
+++ WebApiAuthentication.Server/AuthenticateAttiribute.cs
@@ -1,15 +1,56 @@
+using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Web.Http.Controllers;
 using System.Web.Http.Filters;
 
 namespace WebApiAuthentication.Server
 {
     public class AuthenticateAttiribute : ActionFilterAttribute
     {
-        public override void OnActionExecuting(System.Web.Http.Controllers.HttpActionContext actionContext)
+        public IBuildRequestSignature BuildRequestSignature { get; set; }
+        public IGetSecretFromUsername GetSecretFromUsername { get; set; }
+
+        public AuthenticateAttiribute()
         {
-            var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
-            actionContext.Response = response;
+            BuildRequestSignature = new BuildRequestSignature();
+        }
+
+        public override void OnActionExecuting(HttpActionContext actionContext)
+        {
+            if (!isAuthenticated(actionContext))
+            {
+                var response = new HttpResponseMessage(HttpStatusCode.Unauthorized);
+                actionContext.Response = response;
+            }
+
+            base.OnActionExecuting(actionContext);
+        }
+
+        private bool isAuthenticated(HttpActionContext actionContext)
+        {
+            if (actionContext.Request.Headers.Authorization == null)
+                return false;
+
+            if (!actionContext.Request.Headers.Contains(HeaderNames.UsernameHeader))
+                return false;
+
+            var usernameHeader = actionContext.Request.Headers.GetValues(HeaderNames.UsernameHeader)
+                .FirstOrDefault();
+
+            var secret = GetSecretFromUsername.Secret(usernameHeader);
+
+            if (secret == null)
+                return false;
+
+            var headerSignature = actionContext.Request.Headers.Authorization.Parameter;
+
+            var signature = BuildRequestSignature.Build(secret, actionContext.Request);
+
+            if (signature != headerSignature)
+                return false;
+
+            return true;
         }
     }
 }
