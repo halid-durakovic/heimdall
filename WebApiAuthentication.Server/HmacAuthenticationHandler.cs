4bfe8160d1e0e6f162643c8ece8fd9ad1139105f
Alex Brown <alex.brown@collinsontech.com>
2013-09-24 17:27:48 +0100

    nicer message on 401

--- WebApiAuthentication.Server/HmacAuthenticationHandler.cs
+++ WebApiAuthentication.Server/HmacAuthenticationHandler.cs
@@ -2,8 +2,6 @@
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
-using System.Web.Http;
-using System.Web.Http.Dispatcher;
 
 namespace WebApiAuthentication.Server
 {
@@ -12,20 +10,15 @@ namespace WebApiAuthentication.Server
         private readonly IAuthenticateRequest authenticateRequest;
 
         public HmacAuthenticationHandler(IAuthenticateRequest authenticateRequest)
-            : this(authenticateRequest, GlobalConfiguration.Configuration)
-        { }
-
-        public HmacAuthenticationHandler(IAuthenticateRequest authenticateRequest, HttpConfiguration httpConfiguration)
         {
             this.authenticateRequest = authenticateRequest;
-            InnerHandler = new HttpControllerDispatcher(httpConfiguration);
         }
 
         protected async override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, System.Threading.CancellationToken cancellationToken)
         {
             if (!authenticateRequest.IsAuthenticated(request))
             {
-                var response = request.CreateErrorResponse(HttpStatusCode.Unauthorized, "x");
+                var response = request.CreateErrorResponse(HttpStatusCode.Unauthorized, "Unauthorized API call");
                 response.Headers.WwwAuthenticate.Add(new AuthenticationHeaderValue(HeaderNames.AuthenticationScheme));
                 return response;
             }
