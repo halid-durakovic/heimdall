27d2f6ac5a0892001c6d05b49e9233781d298619
Alex Brown <alex.brown@collinsontech.com>
2013-09-27 12:34:05 +0100

    added IISServer example proj

---
+++ WebApiAuthentication.Examples.IISServer/App_Start/WebApiConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web.Http;
+
+namespace WebApiAuthentication.Examples.IISServer
+{
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+        }
+    }
+}
---
+++ WebApiAuthentication.Examples.IISServer/Global.asax.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Http;
+using System.Web.Mvc;
+using System.Web.Routing;
+using WebApiAuthentication.Server;
+
+namespace WebApiAuthentication.Examples.IISServer
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+    // visit http://go.microsoft.com/?LinkId=9394801
+
+    public class WebApiApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            AreaRegistration.RegisterAllAreas();
+
+            WebApiConfig.Register(GlobalConfiguration.Configuration);
+            FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+
+            var authenticateRequest = new AuthenticateRequest(new DummyGetSecretFromUsername());
+            GlobalConfiguration.Configuration.MessageHandlers.Add(new HmacAuthenticationHandler(authenticateRequest));
+        }
+    }
+
+    public class DummyGetSecretFromUsername : IGetSecretFromUsername
+    {
+        public string Secret(string username)
+        {
+            return "secret";
+        }
+    }
+}
