25fe9a2a4421fb56bef5a5771e82abcd9d04c06d
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2015-07-21 15:26:22 +0100

    Got the nugets packing but not releasing yet

---
+++ BuildTasks/BumpVersion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+using System.Xml;
+using Microsoft.Build.Framework;
+
+namespace BuildTasks
+{
+    public class BumpVersion : ITask
+    {
+        [Required]
+        public string FilePath { get; set; }
+
+        [Required]
+        public string XPathToVersionNumber { get; set; }
+
+        public bool Execute()
+        {
+            var xmlDocument = new XmlDocument();
+            xmlDocument.Load(FilePath);
+
+            var node = xmlDocument.SelectNodes(XPathToVersionNumber).Cast<XmlNode>().FirstOrDefault();
+
+            if (node == null)
+                throw new Exception("Think your xpath is wrong ... ");
+
+            var nodeContent = node.InnerText;
+            var versionNumbers = nodeContent.Split('.');
+            var bumpVersion = int.Parse(versionNumbers.Last()) + 1;
+            versionNumbers[versionNumbers.Length - 1] = bumpVersion.ToString();
+            node.InnerText = string.Join(".", versionNumbers);
+
+            xmlDocument.Save(FilePath);
+
+            return true;
+        }
+
+        public IBuildEngine BuildEngine { get; set; }
+        public ITaskHost HostObject { get; set; }
+    }
+}
