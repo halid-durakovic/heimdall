1ce0f0e1876fe4fd9d11899c1f00cd0145ae8e85
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2015-07-24 15:36:28 +0100

    Upgraded build primer for better versioning when it comes to nuget

---
+++ BuildTasks/MigratePackageDependenciesToNuSpec.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Microsoft.Build.Framework;
+
+namespace BuildTasks
+{
+    public class MigratePackageDependenciesToNuSpec : ITask
+    {
+        [Required]
+        public string RelativePath { get; set; }
+
+        public bool Execute()
+        {
+            var currentDirectory = Path.Combine(Environment.CurrentDirectory, RelativePath);
+
+            var files = Directory.GetFiles(currentDirectory, "packages.config", SearchOption.AllDirectories);
+
+            foreach (var packageConfigFile in files)
+            {
+                var potentialNuSpecFile = packageConfigFile.ToLower().Replace("packages.config", "nuget.nuspec");
+                if (File.Exists(potentialNuSpecFile))
+                {
+                    var packageDocument = new XmlDocument();
+                    packageDocument.Load(packageConfigFile);
+
+                    var packageNodes = packageDocument.SelectNodes("/packages/package");
+                    foreach (XmlNode packageNode in packageNodes)
+                    {
+                        var packageIdentity = packageNode.Attributes["id"].Value;
+                        var packageVersion = packageNode.Attributes["version"].Value;
+
+                        var nuspecDocument = new XmlDocument();
+                        nuspecDocument.Load(potentialNuSpecFile);
+
+                        var dependencyXPath = string.Format("/package/metadata/dependencies/dependency[@id='{0}']", packageIdentity);
+                        var nuspecDependencies = nuspecDocument.SelectNodes(dependencyXPath).Cast<XmlNode>().ToList();
+
+                        if (!nuspecDependencies.Any())
+                        {
+                            var dependencies = nuspecDocument.SelectNodes("/package/metadata/dependencies").Cast<XmlNode>().First();
+                            var newDependency = nuspecDocument.CreateElement("dependency");
+                            newDependency.SetAttribute("id", packageIdentity);
+                            newDependency.SetAttribute("version", packageVersion);
+                            dependencies.AppendChild(newDependency);
+
+                        }
+                        else
+                        {
+                            var existingDependency = nuspecDependencies.First();
+                            existingDependency.Attributes["version"].Value = packageVersion;
+                        }
+
+                        nuspecDocument.Save(potentialNuSpecFile);
+                    }
+                }
+            }
+
+            return true;
+        }
+
+        public IBuildEngine BuildEngine { get; set; }
+        public ITaskHost HostObject { get; set; }
+    }
+}
---
+++ BuildTasks/SetExplicitVersionsForDependencies.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Xml;
+using Microsoft.Build.Framework;
+
+namespace BuildTasks
+{
+    public class NuspecPackage
+    {
+        public string Id { get; set; }
+        public string Version { get; set; }
+    }
+
+    public class SetExplicitVersionsForDependencies : ITask
+    {
+        [Required]
+        public string RelativePath { get; set; }
+
+        public bool Execute()
+        {
+            var packages = new List<NuspecPackage>();
+
+            var currentDirectory = Path.Combine(Environment.CurrentDirectory, RelativePath);
+
+            var files = Directory.GetFiles(currentDirectory, "nuget.nuspec", SearchOption.AllDirectories);
+
+            foreach (var nuspecFile in files)
+            {
+                var nuspecDocument = new XmlDocument();
+                nuspecDocument.Load(nuspecFile);
+
+                var idNode = nuspecDocument.SelectNodes("/package/metadata/id").Cast<XmlNode>().First();
+                var versionNode = nuspecDocument.SelectNodes("/package/metadata/version").Cast<XmlNode>().First();
+
+                packages.Add(new NuspecPackage()
+                {
+                    Id = idNode.InnerText,
+                    Version = versionNode.InnerText
+                });
+            }
+
+            foreach (var nuspecFile in files)
+            {
+                var nuspecDocument = new XmlDocument();
+                nuspecDocument.Load(nuspecFile);
+
+                foreach (var package in packages)
+                {
+                    var dependencyXPath = string.Format("/package/metadata/dependencies/dependency[@id='{0}']", package.Id);
+                    var nuspecDependencies = nuspecDocument.SelectNodes(dependencyXPath).Cast<XmlNode>().ToList();
+
+                    if (nuspecDependencies.Any())
+                    {
+                        XmlNode dependency = nuspecDependencies.First();
+                        dependency.Attributes["version"].Value = package.Version;
+                    }
+                }
+
+                nuspecDocument.Save(nuspecFile);
+            }
+
+            return true;
+        }
+
+        public IBuildEngine BuildEngine { get; set; }
+        public ITaskHost HostObject { get; set; }
+    }
+}
