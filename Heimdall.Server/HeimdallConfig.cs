2955c68869852590004720de18cd880f0926171e
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2016-04-20 01:44:23 +0100

    Allowed the ignoring of verbs

--- Heimdall.Server/HeimdallConfig.cs
+++ Heimdall.Server/HeimdallConfig.cs
@@ -2,16 +2,28 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Cache;
 using System.Net.Http;
+using System.Web;
 
 namespace Heimdall.Server
 {
     public class HeimdallConfig
     {
+        private static List<string> InsecureVerbs = new List<string>();
         private static List<string> InsecurePaths = new List<string>();
-         
+
+        public static void AllowVerb(string verb)
+        {
+            InsecureVerbs.Add(verb?.ToUpper());
+        }
+
         public static void AllowPath(string path)
         {
             InsecurePaths.Add(path);
+        }
+
+        internal static bool IgnoreVerb(HttpRequestMessage message)
+        {
+            return InsecurePaths.Any(x => HttpContext.Current.Request.HttpMethod.ToUpper().ToLower().StartsWith(x));
         }
 
         internal static bool IgnorePath(HttpRequestMessage message)
