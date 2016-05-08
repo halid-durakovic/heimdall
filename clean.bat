779f73e0ff22722fc7f55f94f9ada9bed6be2146
Gavin van der Merwe <fir3pho3nixx@gmail.com>
2016-05-08 22:31:18 +0100

    Added clean file

---
+++ clean.bat
@@ -0,0 +1,4 @@
+@echo off
+FOR /D %%p IN (".\packages\*.*") DO rmdir "%%p" /s /q
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S bin') DO RMDIR /S /Q "%%G"
+FOR /F "tokens=*" %%G IN ('DIR /B /AD /S obj') DO RMDIR /S /Q "%%G"
